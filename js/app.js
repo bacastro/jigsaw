b7cf619f638c559cf183fff7d39396f35835992a
Alejandro Castro <bacastro1@hotmail.com>
2022-06-14 21:36:13 -0500

    Primer Commit para subir a GitHub

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//Selección de la dificultad
+const facil = document.querySelector('#facil');
+const medio = document.querySelector('#medio');
+const dificil = document.querySelector('#dificil');
+
+
