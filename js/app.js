49e194659424678f37fd691e342230e3689eeccd
Alejandro Castro <bacastro1@hotmail.com>
2022-06-20 19:04:54 -0500

    Agregando modulos:Imagenes-Rompecabezas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 //Selección de la dificultad
-const facil = document.querySelector('#facil');
-const medio = document.querySelector('#medio');
-const dificil = document.querySelector('#dificil');
+const dificultad = document.querySelector('#dificultad')
+let dificultadSeleccionada; 
+
+document.addEventListener('DOMContentLoaded',()=>{
+    dificultad.addEventListener('click', seleccionarDificultad);
+})
 
 
+function seleccionarDificultad(e){
+    e.preventDefault();
+    if(e.target.classList.contains('btn-select')){
+        dificultadSeleccionada = e.target.getAttribute('data-id');
+    }
+
+    if(dificultadSeleccionada === "1"){
+        window.open(`http://127.0.0.1:5500/facil.html`, "_self")
+    }
+
+}
