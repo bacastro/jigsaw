4b967b9a0f237d03fea37e4486983d9d279c4ac7
Alejandro Castro <bacastro1@hotmail.com>
2022-06-25 14:02:11 -0500

    Modificando el codigo a POO

--- js/rompecabezas.js
+++ js/rompecabezas.js
@@ -1,6 +1,7 @@
 const dificultad = document.querySelector('.contenedor').getAttribute('data-dificultad');
-const btnConfirmar = document.querySelector('#btn-confirmar');
 let piezas = document.querySelectorAll('.pieza');
+const btnConfirmar = document.querySelector('#btn-confirmar');
+
 let infFichaSeleccionada=null;
 let infFichaCambiar=null;
 let numFichaCambiar;
@@ -12,37 +13,70 @@ let idFichaCambiar;
 let agregarBorde;
 let quitarBorde;
 
+
 document.addEventListener('DOMContentLoaded', () =>{
-    desordenarRompecabezas(dificultad);
+    desordenar.desordenarRompecabezas();
     btnConfirmar.addEventListener('click', verificarRompecabezas);
 })
 
-function desordenarRompecabezas(dificultad){
-    let valor=arrayTamano(dificultad);
-    let imagenMuestra= document.querySelector('.imagen-muestra img');
-    let aleatorio;  
 
-    if(dificultad==='facil'){
-        aleatorio = Math.ceil(Math.random()*3);
-        valor.sort(function() { return Math.random() - 0.5 });
-        imagenMuestra.setAttribute('src',`./img/rompecabezas${aleatorio}/10.png`)
-    }else if(dificultad === 'medio'){
-        aleatorio = 4;
-        valor.sort(function() { return Math.random() - 0.5 });
-        imagenMuestra.setAttribute('src',`./img/rompecabezas${aleatorio}/17.png`)
-    }else if(dificultad === 'dificil'){
-        aleatorio = 5;
-        valor.sort(function() { return Math.random() - 0.5 });
-        imagenMuestra.setAttribute('src',`./img/rompecabezas${aleatorio}/26.png`)
+//Clase
+class Rompecabezas{
+
+    constructor(dificultad, piezas){
+        this.dificultad = dificultad;
+        this.piezas = piezas
     }
 
-    for (let i = 0; i < piezas.length; i++) {
-        piezas[i].src=`./img/rompecabezas${aleatorio}/${valor[i]}.png`;
-        piezas[i].setAttribute('dataid',`pieza${valor[i]}`);
-        piezas[i].addEventListener('click',seleccionarFicha);
+    desordenarRompecabezas(){
+
+        let valor=this.arrayTamano(this.dificultad);
+        let imagenMuestra= document.querySelector('.imagen-muestra img');
+        let aleatorio;  
+    
+        if(this.dificultad==='facil'){
+            aleatorio = Math.ceil(Math.random()*3);
+            valor.sort(function() { return Math.random() - 0.5 });
+            imagenMuestra.setAttribute('src',`./img/rompecabezas${aleatorio}/10.png`)
+        }else if(this.dificultad === 'medio'){
+            aleatorio = 4;
+            valor.sort(function() { return Math.random() - 0.5 });
+            imagenMuestra.setAttribute('src',`./img/rompecabezas${aleatorio}/17.png`)
+        }else if(this.dificultad === 'dificil'){
+            aleatorio = 5;
+            valor.sort(function() { return Math.random() - 0.5 });
+            imagenMuestra.setAttribute('src',`./img/rompecabezas${aleatorio}/26.png`)
+        }
+    
+        for (let i = 0; i < piezas.length; i++) {
+            this.piezas[i].src=`./img/rompecabezas${aleatorio}/${valor[i]}.png`;
+            this.piezas[i].setAttribute('dataid',`pieza${valor[i]}`);
+            this.piezas[i].addEventListener('click',seleccionarFicha);
+        }
     }
 
+    arrayTamano(dificultad){
+        const arr1 = [];
+        let tamano;
+        if(dificultad === "facil"){
+            tamano = 9;
+        }else if(dificultad === "medio"){
+            tamano = 16;
+        } else if(dificultad === "dificil"){
+            tamano = 25
+        }
+    
+        for (let z = 0; z < tamano; z++) {
+            arr1.push(z+1);
+        }
+    
+        return arr1;
+    }
 }
+
+//Instanciar
+const desordenar = new Rompecabezas(dificultad, piezas);
+
 
 function seleccionarFicha(e){
     e.preventDefault();
@@ -75,7 +109,7 @@ function seleccionarFicha(e){
 }
 
 function verificarRompecabezas(){
-    /* e.preventDefault(); */
+
     let valorId;
     let valorDataid;
     let resultado;
@@ -87,8 +121,10 @@ function verificarRompecabezas(){
         valorDataid=piezas[i].attributes.dataid.value;
         if(valorId === valorDataid){
             resultado=true;
+            console.log(resultado)
         }else{
             resultado=false
+            console.log(resultado)
             break;
         }
     }
@@ -101,25 +137,3 @@ function verificarRompecabezas(){
         btnContinuar.classList.add('disable');
     }
 }
-
-function arrayTamano(dif){
-    const arr1 = [];
-    let tamano;
-    if(dif === "facil"){
-        tamano = 9;
-    }else if(dif === "medio"){
-        tamano = 16;
-    } else if(dif === "dificil"){
-        tamano = 25
-    }
-
-    for (let z = 0; z < tamano; z++) {
-        arr1.push(z+1);
-    }
-
-    return arr1;
-}
-
-
-
-
